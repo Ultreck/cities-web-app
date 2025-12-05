8668ae31cb07f839383a5e0d4ac1fa06282fa798
Oluwadunsin <oluwadunsin2016@gmail.com>
2025-12-05 12:36:33 +0100

    I fixed the post error

--- apis/postMutation.ts
+++ apis/postMutation.ts
@@ -1,11 +1,19 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import clientApi from "./clientApi";
+import axios from "axios";
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+ const token = typeof window !== "undefined" ? localStorage.getItem("auth_token") : null;
+ const bearerToken =`Bearer ${token}`
 
 
 export const useCreatePostMutation = () => {
    const queryClient=useQueryClient()
   return useMutation({
-    mutationFn: (payload: FormData) => clientApi.post('/post/create', payload),
+    mutationFn: (payload: FormData) => {
+         return axios.post(baseUrl+"/post/create", payload,{headers:{
+    "Content-Type": "multipart/form-data", "Authorization":bearerToken
+  }});
+    },
     onSuccess:()=>{
       queryClient.invalidateQueries({queryKey: ["posts"]})
     }
